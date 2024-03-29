b735f642f4af6c85621e68b6d7479e2aa1793b7a
Sayed Husain Alawi <shalawi@mail.fhsu.edu>
2024-03-29 11:54:39 +0300

    Updated @bp.route('/') under blog.py file

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -13,9 +13,9 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
+        'SELECT mr.id, movie_name, actors, director, length, genre, rating, review, mr.created, mr.author_id, username'
+        ' FROM movie_review mr JOIN user u ON mr.author_id = u.id'
+        ' ORDER BY mr.created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
 
